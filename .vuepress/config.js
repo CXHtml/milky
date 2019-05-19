3e371b0221cd316164408af0db7a37c093122adf
Chester Chen <lanhai723@126.com>
2019-05-20 00:46:58 +0800

    :sparkles:add .vuepress config

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: '牛奶小镇',
+    description: '代码、音乐、猫、还有爱',
+    themeConfig: {
+    nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+        ],
+    },
+};
